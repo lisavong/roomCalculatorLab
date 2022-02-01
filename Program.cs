e891780fa8e945d1d8e8c8967fbb85dab84a754d
lisavong <83181082+lisavong@users.noreply.github.com>
2022-01-31 21:53:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AHBC_Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the length of the room in feet");
+            int length = Convert.ToInt32(Console.ReadLine());
+            
+            
+            Console.WriteLine("enter the width of the room in feet");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            int parCalc = 2*length + 2*width;
+
+            int areaCalc = length * width;
+
+            Console.WriteLine("Parameter of the room = " + parCalc);
+            Console.WriteLine("Area of the room = " + areaCalc);
+
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
